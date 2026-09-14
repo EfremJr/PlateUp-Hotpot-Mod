648a99dc9e11f8b69b73ba237a4c30b169aa16ca
EfremJr <efremarrivas.jr@gmail.com>
2026-09-14 02:44:16 -0700

    Initial commit - mod set up and basic GDOs

---
+++ MisoBroth.cs
@@ -0,0 +1,127 @@
+using System.Collections.Generic;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using KitchenLib.References;
+using UnityEngine;
+using KitchenData;
+using Kitchen;
+using KitchenLib.Materials;
+
+namespace HotpotMod
+{
+    public class MisoPacket : CustomItem
+    {
+        public override string UniqueNameID => "MisoPacket";
+
+        public override ItemCategory ItemCategory => ItemCategory.Generic;
+        public override ItemStorage ItemStorageFlags => ItemStorage.Small;
+        //public override Appliance DedicatedProvider => ???
+        public override GameObject Prefab => null;
+    }
+
+    public class MisoPacketShelf : CustomAppliance
+    {
+        public override string UniqueNameID => "MisoPacketShelf";
+        public override List<(Locale, ApplianceInfo)> InfoList => new List<(Locale, ApplianceInfo)>
+        {
+            (
+                Locale.English,
+                new ApplianceInfo
+                {
+                    Name = "Miso Packet Shelf", 
+                    Description = "Provides Miso Packets"
+                }
+            )
+        };
+    }
+
+
+    public class UncookedMisoBroth : CustomItemGroup
+    {
+        public override string UniqueNameID => "UncookedMisoBroth";
+
+        public override List<ItemGroup.ItemSet> Sets => new List<ItemGroup.ItemSet>
+        {
+          new ItemGroup.ItemSet
+          {
+              Max = 3,
+              Min = 3,
+              IsMandatory = true,
+              Items = new List<Item>
+              {
+                  (Item)GDOUtils.GetExistingGDO(ItemReferences.Water),
+                  (Item)GDOUtils.GetExistingGDO(ItemReferences.Pot),
+                  (Item)GDOUtils.GetCustomGameDataObject<MisoPacket>().GameDataObject
+              }
+          }  
+        };
+
+        public override List<Item.ItemProcess> Processes => new List<Item.ItemProcess>
+        {
+            new Item.ItemProcess
+            {
+                Process = (Process)GDOUtils.GetExistingGDO(ProcessReferences.Cook),
+                Result = (Item)GDOUtils.GetCustomGameDataObject<CookedMisoBroth>().GameDataObject,
+                Duration = 10f,
+                IsBad = false
+            }    
+        };
+
+        public override GameObject Prefab => null;
+        //modeled as packet or pile in water
+        public override void OnRegister(ItemGroup gameDataObject)
+        {
+            ItemGroup basePot = (ItemGroup)GDOUtils.GetExistingGDO(ItemReferences.Pot);
+
+            GameObject clonedPot = GameObject.Instantiate(basePot.Prefab);
+            clonedPot.name = "UncookedMisoBroth_Prefab";
+            
+            MeshRenderer[] renderers = clonedPot.GetComponentsInChildren<MeshRenderer>();
+            foreach (MeshRenderer renderer in renderers)
+            {
+                if (renderer.gameObject.name.Contains("Water") || renderer.gameObject.name.Contains("Liquid"))
+                {
+                    renderer.material = MaterialManager.GetMaterial("Soup - Meat");
+                }
+
+            }
+            gameDataObject.Prefab = clonedPot;
+        }
+    }
+    public class CookedMisoBroth : CustomItem
+    {
+        public override string UniqueNameID => "CookedMisoBroth";
+
+        public override ItemStorage ItemStorageFlags => ItemStorage.Dish;
+
+        public override int SplitCount => 4;
+
+        public override Item DisposesTo => (Item)GDOUtils.GetExistingGDO(ItemReferences.Pot);
+
+        //public override Item SplitSubItem => (Item)GDOUtils.GetCustomGameDataObject<MisoServingPot>().GameDataObject;
+
+        public override GameObject Prefab => null;
+
+        //figure out texture stuff later
+        public override void OnRegister(Item gameDataObject)
+        {
+            ItemGroup basePot = (ItemGroup)GDOUtils.GetExistingGDO(ItemReferences.Pot);
+
+            GameObject clonedPot = GameObject.Instantiate(basePot.Prefab);
+            clonedPot.name = "CookedMisoBroth_Prefab";
+
+            MeshRenderer[] renderers = clonedPot.GetComponentsInChildren<MeshRenderer>();
+            foreach (MeshRenderer renderer in renderers)
+            {
+                if (renderer.gameObject.name.Contains("Water") || renderer.gameObject.name.Contains("Liquid"))
+                {
+                    renderer.material = MaterialManager.GetMaterial("Soup - Meat");
+                }
+
+            }
+            gameDataObject.Prefab = clonedPot;
+        }
+    }
+}
+
+
---
+++ main.cs
@@ -0,0 +1,36 @@
+﻿using KitchenMods;
+using KitchenLib;
+using UnityEngine;
+using System.Reflection;
+using KitchenLib.Logging;
+using System.Linq;
+
+namespace HotpotMod
+{
+    public class Main : BaseMod, IModSystem
+    {
+        // Mod Metadata
+        public const string MOD_GUID = "com.night.hotpotmod";
+        public const string MOD_NAME = "Hotpot Mod";
+        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_AUTHOR = "Night";
+        public const string MOD_GAMEVERSION = ">=1.5.0";
+
+        public Main() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly())
+        {
+            AddGameDataObject<MisoPacket>();
+            AddGameDataObject<UncookedMisoBroth>();
+            AddGameDataObject<CookedMisoBroth>();
+        }
+
+        //internal static AssetBundleModPack bundle;
+        internal static KitchenLogger Logger;
+
+        protected override void OnPostActivate(Mod mod)
+        {
+            Logger = InitLogger();
+            
+            //bundle = mod.GetPacks<AssetBundleModPack>().SelectMany(e => e.AssetBundles).FirstOrDefault() ?? throw new MissingAssetBundleException(MOD_GUID);
+        }
+    }
+}
